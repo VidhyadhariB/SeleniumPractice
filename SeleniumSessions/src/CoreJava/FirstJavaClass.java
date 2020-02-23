dad57c044c45e2ade9e214aa84ae2486404a107c
Rajesh <Rajesh@MININT-UM25UFB>
2020-02-23 13:10:22 -0500

    second commit

---
+++ SeleniumSessions/src/CoreJava/FirstJavaClass.java
@@ -0,0 +1,20 @@
+package CoreJava;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the java class");
+		fjc();
+		String name = fjc1() ;
+
+	}
+	public static void fjc () {
+		System.out.println("This is a fcj method");
+	}
+	public static String fjc1() {
+		System.out.println("This is the fjc1 method");
+		return "vidhya";
+
+	}
+
+}
